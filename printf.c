6514c31e71dd4f06d8d8f9cbd28ff5a2317426e0
EG-AKANDE <emmanuel_akande@yahoo.com>
2023-04-24 13:57:51 -0700

    printf edit

--- printf.c
+++ printf.c
@@ -1,52 +1,43 @@
-#include “main.h”
+#include "main.h"
 
 /**
  * _printf - Function that produces output according to a given format
  * @format: Output format
- *
- * Return: On success len.
-*/
+ * Return: On success, the number of characters printed.
+ */
 
+/* Defining the function _printf and its parameters */
 int _printf(const char *format, ...)
 {
-	int len = 0;
-	va_list args;
+/* Initializing variables */
+int len = 0;
+va_list args;
 
-	va_start(args, format);
-	while (*format)
+/* Starting the list of arguments */
+va_start(args, format);
+
+/* Looping through the format string */
+while (*format)
+{
+	/* Checking if the current character is a format specifier */
+	if (*format == '%')
 	{
-		if (*format == '%')
-		{
-			format++;
-			if (*format == 'c')
-			{
-				char c = va_arg(args, int);
-
-				len += putchar(c);
-			}
-			else if (*format == 's')
-			{
-				char *s = va_arg(args, char *);
-
-				len += print_string(s);
-			}
-			else if (*format == '%')
-			{
-				len += _putchar('%');
-			}
-			else if (*format == 'd' || *format == 'i')
-			{
-				int num = va_arg(args, int);
-
-				len += print_intteger(num);
-			}
-		}
-		else
-		{
-			len += _putchar(*format);
-		}
+		format++;
+		/* Adding the length returned by handle_format_specifier to len */
+		len += handle_format_specifier(&format, args);
+	}
+	/* If the current character is not a format specifier */
+	else
+	{
+		/* Printing the character and adding 1 to len */
+		len += print_char(*format);
+		/* Moving to the next character in the format string */
 		format++;
 	}
-	va_end(args);
-	return (len);
+}
+
+/* Ending the list of arguments */
+va_end(args);
+/* Returning the number of characters printed */
+return (len);
 }
