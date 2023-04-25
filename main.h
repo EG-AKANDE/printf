f68bbb9587dad17686e8e69ca881c3e519c3c183
Inye Allison <amazingfred4real@gmail.com>
2023-04-25 00:52:39 +0000

    Add some prototypes to main.h file

--- main.h
+++ main.h
@@ -6,5 +6,11 @@
 
 
 int _printf(const char *format, ...);
+int _putchar(char c);
+int print_string(char *s);
+int print_integer(int num);
+int format_specifier(const char **format, va_list args);
+int print_binary(unsigned int num);
+
 
 #endif
